afc3a93ad6412be3e9657202486d4dbea4487c45
mj <info@lemoni.ir>
2024-05-19 20:18:15 +0330

    improve structure and varaible

--- cmd/post_install.php
+++ cmd/post_install.php
@@ -1,5 +1,2 @@
 <?php
-fwrite(fopen(__DIR__ . '/' . __LINE__ . '.txt', 'w+'), '' . __LINE__);
-fwrite(fopen(dirname(__DIR__) . '/' . __LINE__ . '.txt', 'w+'), '' . __LINE__);
-fwrite(fopen(dirname(__DIR__, 2) . '/' . __LINE__ . '.txt', 'w+'), '' . __LINE__);
-//fwrite(fopen(dirname(__DIR__, 3) . '/' . __LINE__ . '.txt', 'w+'), '' . __LINE__);
+echo "123";
