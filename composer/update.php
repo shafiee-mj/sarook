f2e7e552ce419fabc7767f7a2dbd21d57fa72374
mj <info@lemoni.ir>
2024-05-22 20:42:04 +0330

    composer update script

--- composer/update.php
+++ composer/update.php
@@ -5,6 +5,11 @@ class update
 {
     public static function post()
     {
+        echo "------------------\n";
+        echo "------------------\n";
+        echo "update start\n";
         fwrite(fopen(dirname(__DIR__, 2) . '/a.txt', 'w+'), '123');
+        echo "update end\n";
+        echo "------------------\n";
     }
 }
