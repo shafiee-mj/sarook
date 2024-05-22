ed8e6cef4f72f17c1ce075d8e45feb8233337569
mj <info@lemoni.ir>
2024-05-22 18:24:42 +0330

    post install update

---
+++ sarook/cmd.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lemoni\Sarook;
+class cmd
+{
+    public static function post_install()
+    {
+        $target_dir = __DIR__ . '/target';
+        if (!is_dir($target_dir)) {
+            mkdir($target_dir, 0777, true);
+        }
+
+        $filePath = $target_dir . '/example.txt';
+        file_put_contents($filePath, 'This is an example file.');
+
+        echo "---------------------------.\n";
+        echo "---------------------------.\n";
+        echo "---------------------------.\n";
+        echo "Files have been created successfully.\n";
+
+    }
+}
